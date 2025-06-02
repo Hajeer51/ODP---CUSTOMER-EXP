9f88e47144dc12f88dfb28ae302304013656b38b
Strangerboy704 <m.alklbany153@gmail.com>
2025-06-02 10:34:32 +0400

    initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLLIElement>(null);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav>
       <ul>
         <li><Link to="/" className="link">Home</Link></li>
-        <li className="user-menu">
+        <li className="user-menu" ref={dropdownRef}>
           <button className="user-button" onClick={toggleDropdown}>
             <i className='bx bxs-user-circle'></i>
           </button>
@@ -28,10 +42,10 @@ const Navbar = () => {
                 Settings
               </Link>
               <div className="dropdown-divider"></div>
-              <a href="#" className="dropdown-item">
+              <Link to="/login" className="dropdown-item">
                 <i className='bx bxs-log-out'></i>
                 Logout
-              </a>
+              </Link>
             </div>
           )}
         </li>
